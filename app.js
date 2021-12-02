d0bfab5fbe832544d1b18902e2399799c4415404
Jatin Thakwani <47001242+jatin1408@users.noreply.github.com>
2021-12-02 19:56:18 +0530

    added db changes

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize');
+
 
 app.listen(3000);
 
 app.get('/',function(req,res){
     res.status(200).send("SpringRangers");
 })
+
+const sequelize = new Sequelize('burst', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+
+async function testConnection()
+{
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+
+testConnection()
