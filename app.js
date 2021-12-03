02b79227e6539318e220653687b4a5ec14177672
madhvimit123 <madhvi.mittal.1998@gmail.com>
2021-12-03 13:07:53 +0530

    files structure

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/',function(req,res){
 })
 
 app.use('/api', router.api);
-const sequelize = new Sequelize('burst', 'root', 'root', {
+const sequelize = new Sequelize('safepay', 'root', 'root', {
     host: 'localhost',
     dialect: 'mysql'
 });
